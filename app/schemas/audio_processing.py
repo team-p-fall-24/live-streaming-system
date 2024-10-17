e4f69c6c09bc6a14c94048816cf93f4b5b0406fd
Minh Luong <62457484+minhlgdo@users.noreply.github.com>
2024-10-17 22:44:52 +0900

    feat: process audio and audio files

--- app/schemas/audio_processing.py
+++ app/schemas/audio_processing.py
@@ -1,4 +1,4 @@
 from pydantic import BaseModel, HttpUrl
 
 class AudioProcessingRequest(BaseModel):
-    stream_url: HttpUrl
+    stream_url: str
--- app/services/audio_service.py
+++ app/services/audio_service.py
@@ -1,7 +1,36 @@
 import subprocess
+import threading
+import glob
+import time
+import librosa
 
+AUDIO_OUTPUT = "app/output"
+
+# Processes an individual audio file: transcribes, writes subtitles, and deletes the file once donee processing
+def process_audio_file(file):
+    print(f"Processing file {file}")
+    return
+
+# Continuously monitors and processes new audio files
+def process_audio_files():
+    processed_files = set()
+    while True:
+        files = glob.glob(f"{AUDIO_OUTPUT}/audio_*.wav")
+        for file in files:
+            if file not in processed_files:
+                duration = librosa.get_duration(path=file)
+                # Only process audio files whose length is over 9 seconds
+                if duration > 9:
+                    print(f"File {file} has duration {duration}")
+                    processed_files.add(file)
+                    process_audio_file(file)
+        time.sleep(1)
+
+
+# Extracts audio from the stream and segments it for processing
 def process_audio(stream_url: str):
     try:
+        # FFmpeg command to segment the audio every 10 seconds
         command = [
             "ffmpeg",
             "-i", stream_url,
@@ -9,9 +38,23 @@ def process_audio(stream_url: str):
             "-acodec", "pcm_s16le",  # WAV format
             "-ar", "44100",  # Sample rate
             "-ac", "2",  # Stereo
-            "output_audio.wav"
+            "-f", "segment", # Segment
+            "-segment_time", "10",
+            "-strftime", "1",
+            "-reset_timestamps", "1",
+            f"{AUDIO_OUTPUT}/audio_%Y%m%d%H%M%S.wav"
         ]
-        subprocess.run(command, check=True)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(f"Audio extracted from {stream_url}")
+
+        # Start the background thread to process audio files
+        print("Start processing audio files...")
+        threading.Thread(target=process_audio_files, daemon=True).start()
+        
+        # Wait for the FFmpeg process to complete
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            print(f"FFmpeg error: {stderr.decode()}")
+
     except subprocess.CalledProcessError as e:
         print(f"Error processing audio: {e}")
